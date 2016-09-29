6f914c1a38e5dcecb1eddea03f1f1a34ecdddcb3
Keionne Derousselle <knnderousselle@gmail.com>
2016-09-28 20:54:46 -0600

    refactoring photobooth video logic

--- photobooth/src/capture.js
+++ photobooth/src/capture.js
@@ -1,13 +1,5 @@
-navigator.getUserMedia = navigator.webkitGetUserMedia
 
-function handleSuccess(videoEl, stream)
-{
-    videoEl.src = window.URL.createObjectURL(stream);
-}
-
-function handleError(error){
-    console.log('Camer error: ', error);
-}
+const video = require('./video');
 
 window.addEventListener('DOMContentLoaded', () => {
     const videoEl = document.getElementById('video');
@@ -16,17 +8,5 @@ window.addEventListener('DOMContentLoaded', () => {
     const photoEl = document.querySelector('.photosContainer');
     const counterEl = document.getElementById('counter');
 
-    const constraints = {
-        audio: false,
-        video:{
-            mandatory:{
-                minWidth: 853,
-                minHeight: 480,
-                maxWidth: 853,
-                maxHeight: 480
-            }
-        }
-    };
-
-    navigator.getUserMedia(constraints, stream => handleSuccess(videoEl, stream), error => handleError(error));
+    video.init(navigator, videoEl);
 });
---
+++ photobooth/vendor/video.js
@@ -0,0 +1,25 @@
+    const constraints = {
+        audio: false,
+        video:{
+            mandatory:{
+                minWidth: 853,
+                minHeight: 480,
+                maxWidth: 853,
+                maxHeight: 480
+            }
+        }
+    };
+
+function handleSuccess(videoEl, stream)
+{
+    videoEl.src = window.URL.createObjectURL(stream);
+}
+
+function handleError(error){
+    console.log('Camer error: ', error);
+}
+
+exports.init = (nav, videoEl) => {
+    nav.getUserMedia = navigator.webkitGetUserMedia
+    nav.getUserMedia(constraints, stream => handleSuccess(videoEl, stream), error => handleError(error));
+};
